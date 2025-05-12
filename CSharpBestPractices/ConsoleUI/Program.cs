15af2406116425f48f7054ed4936eb9de3e8da7b
Teodor Yanchev <teo.yanchev93@gmail.com>
2025-05-11 20:47:02 -0400

    Worked on readme file, Program file with writing code and examples.

--- CSharpBestPractices/ConsoleUI/Program.cs
+++ CSharpBestPractices/ConsoleUI/Program.cs
@@ -1,15 +1,28 @@
 ﻿using HelperLibrary;
 
+// Capturing a number of people's names
+// Loop through and say hi to them
+
 namespace ConsoleUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Person firstUser = new Person();
 
-            firstUser.FirstName = "Teodor";
-            firstUser.LastName = "Yanchev";
+            // This is bad way to write code. The Main method do more than one thing.
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person { FirstName = "Teodor", LastName = "Yanchew" });
+            people.Add(new Person { FirstName = "Petko", LastName = "Petkov" });
+            people.Add(new Person { FirstName = "John", LastName = "Conrad" });
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
